7af25b8109e573ac04017946c308ecc33c147828
imran amin <imran.amin@techandover.com>
2022-05-30 00:29:37 +0500

    4

---
+++ database/migrations/2022_05_21_151801_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('veh_image');
+            $table->string('veh_name');
+            $table->string('max_passanger');
+            $table->string('max_luggage');
+            $table->string('veh_extra_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
